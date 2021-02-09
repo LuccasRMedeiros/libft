4fa0109147bf1fcf04869ab7364e47984ebd6a3d
Luccas Rocigno de medeiros <lrocigno@c1r4p2.42sp.org.br>
2021-02-09 01:33:00 -0300

    let's call it a day

--- libft.h
+++ libft.h
@@ -6,20 +6,17 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 14:13:24 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/08 23:51:42 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/09 01:32:01 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
 void	*ft_memset(void *s, int *c, size_t *n);
-
 void	ft_bzero(char *s, size_t n);
-
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-
 void	*ft_memmove(void *dst, const void *src, size_t n);
-
+void	*ft_memchr(const void *str, int c, size_t n);
+int		ft_memcmp(const void *str1, const void *str2, size_t n);
 size_t	ft_strlen(const char *str);
