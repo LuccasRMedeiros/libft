62ad05d397836ba428ea5f47478abda081dc6ff8
Luccas Rocigno de medeiros <lrocigno@c1r1p4.42sp.org.br>
2021-02-11 00:27:43 -0300

    ft_atoi almost there

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,48 +6,60 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 15:53:47 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/10 23:52:04 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/11 00:27:22 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static int	f_signal(int c)
+{
+	if (c == '-')
+	{
+		return (-1);
+	}
+	return (1);
+}
 
 int			ft_atoi(const char *str)
 {
-	long int	cummulus;
-	long int	power;
+	long int	am;
+	long int	uh;
 	int			sig;
+	int			i;
 
-	cummulus = 0;
-	power = 48;
-	while (*str != '\0')
+	am = 0;
+	uh = 0;
+	sig = 1;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (is_digit(*str))
+		if (ft_isdigit(str[i]))
 		{
-			cummulus += *str;
-			power += 48;
+			am *= 10;
+			uh *= 10;
+			am += str[i];
+			uh += 48;
 		}
-		else if (*str == '+' || *str == '-')
+		else if (str[i] == '+' || str[i] == '-')
 		{
-			sig = signal(*str);
+			sig = f_signal(str[i]);
 		}
-		else if (!(ft_isdigit(*str)) && (*str != 32))
+		else if (!(ft_isdigit(str[i])) && (str[i]!= 32))
 		{
-			cummulus = (cummulus - power) * sig;
-			return (cummulus);
+			am = (am - uh) * sig;
+			return (am);
 		}
-		cummulus *= 10;
-		power *= 10;
-		*str++;
+		printf("time \033[1;33m%i\033[0m - amount: \033[1;32m%li\n\033[0m", i, cummulus);
+		i++;
 	}
 	return (0);
 }
 
-static int	signal(int c)
+int	main()
 {
-	if (c == '-')
-	{
-		return (-1)
-	}
-	return (1);
-}	
+	printf("original: %i\n", atoi("     90abca"));
+	printf("ft_atoi: %i\n", ft_atoi("    90abca"));
+	return 0;
+}
