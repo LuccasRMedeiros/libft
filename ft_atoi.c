720db0eae0104c898f2080395adfccb95e675b65
Luccas Rocigno de medeiros <lrocigno@c1r1p4.42sp.org.br>
2021-02-10 23:52:43 -0300

    atoi in development

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,48 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 15:53:47 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/10 15:56:16 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/10 23:52:04 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int			ft_atoi(const char *str)
 {
+	long int	cummulus;
+	long int	power;
+	int			sig;
+
+	cummulus = 0;
+	power = 48;
+	while (*str != '\0')
+	{
+		if (is_digit(*str))
+		{
+			cummulus += *str;
+			power += 48;
+		}
+		else if (*str == '+' || *str == '-')
+		{
+			sig = signal(*str);
+		}
+		else if (!(ft_isdigit(*str)) && (*str != 32))
+		{
+			cummulus = (cummulus - power) * sig;
+			return (cummulus);
+		}
+		cummulus *= 10;
+		power *= 10;
+		*str++;
+	}
+	return (0);
 }
+
+static int	signal(int c)
+{
+	if (c == '-')
+	{
+		return (-1)
+	}
+	return (1);
+}	
