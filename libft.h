bf078481b5f56aac3efcd20b378c5ab9b2dc0132
Luccas Rocigno de medeiros <lrocigno@c1r1p4.42sp.org.br>
2021-02-10 23:11:27 -0300

    almost there

--- libft.h
+++ libft.h
@@ -6,12 +6,11 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 14:13:24 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/10 20:37:51 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/10 23:11:02 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 void	*ft_memset(void *s, int *c, size_t *n);
 void	ft_bzero(char *s, size_t n);
@@ -29,3 +28,7 @@ int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
