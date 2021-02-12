043858ecb7f1899884d288e5a3d1b473e455c686
Luccas Rocigno de medeiros <lrocigno@c1r4p3.42sp.org.br>
2021-02-12 11:44:39 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,13 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 09:48:04 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/11 19:48:18 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/12 11:44:26 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -39,4 +41,25 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	}
 	dst[di] = '\0';
 	return (ds + si);
+}
+
+int		main()
+{
+	char	dst[60] = "mano kk";
+	int		siz = strlen(dst);
+	char	*src = " doenças sexualemnte transmissíveis kkkkkkk";
+	int		r = strlcat(dst, src, 60);
+
+	printf("String: %s\n", dst);
+	printf("Old size: %i\n", siz);
+	printf("New size: %i\n", r);
+	if (siz < r)
+	{
+		puts("\033[1;31m\n -- Trucou -- \n\033[0m");
+	}
+	else
+	{
+		puts("\033[1;32m\n -- Não trucou kkkk -- \n\033[0m");
+	}
+	return 0;
 }
