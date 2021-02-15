9a5adf858b72823883dfa18f264f67998dc60423
Luccas Rocigno de medeiros <lrocigno@c1r2p2.42sp.org.br>
2021-02-15 19:11:43 -0300

    ft_strlcat working fine

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,60 +6,32 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 09:48:04 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/12 11:44:26 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/15 16:51:12 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	si;
-	size_t	di;
+	size_t	i;
 	size_t	ds;
 	size_t	ss;
 
-	si = 0;
-	di = ft_strlen(dst);
-	ds = di;
+	i = 0;
+	ds = ft_strlen(dst);
 	ss = ft_strlen(src);
 	if (dstsize == 0)
 	{
 		return (ss);
 	}
-	else if (dstsize < ds)
+	while (src[i] != '\0' && (i + ds < dstsize - 1))
 	{
-		return (dstsize + ss);
+		dst[i + ds] = src[i];
+		i++;
 	}
-	while (src[si] != '\0' && (si + 1 < dstsize))
-	{
-		dst[di] = src[si];
-		di++;
-		si++;
-	}
-	dst[di] = '\0';
-	return (ds + si);
-}
-
-int		main()
-{
-	char	dst[60] = "mano kk";
-	int		siz = strlen(dst);
-	char	*src = " doenças sexualemnte transmissíveis kkkkkkk";
-	int		r = strlcat(dst, src, 60);
-
-	printf("String: %s\n", dst);
-	printf("Old size: %i\n", siz);
-	printf("New size: %i\n", r);
-	if (siz < r)
-	{
-		puts("\033[1;31m\n -- Trucou -- \n\033[0m");
-	}
-	else
-	{
-		puts("\033[1;32m\n -- Não trucou kkkk -- \n\033[0m");
-	}
-	return 0;
+	dst[i + ds] = '\0';
+	if (dstsize > i)
+		return (i + ss);
+	return (ss + dstsize);
 }
