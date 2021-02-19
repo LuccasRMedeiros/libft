1c829b6c6cb5d1b29cab38c26e56001eac8fb6f4
Luccas Rocigno de medeiros <lrocigno@c1r2p2.42sp.org.br>
2021-02-19 14:26:36 -0300

    lets see

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 16:24:00 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/09 00:09:46 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/19 14:23:36 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
@@ -23,7 +24,19 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	src_aux = (unsigned char*)src;
 	while (i++ < n)
 	{
+		printf("Copying char: \033[1;33m%c\033[0m\n", src_aux[n - i]);
 		dst_aux[n - i] = src_aux[n - i];
+		printf("Received char: \033[1;33m%c\033[0m\n", dst_aux[n - i]);
 	}
-	return (dst_aux);
+	return (dst);
+}
+
+int	main()
+{
+	char *str1 = "TestMother Fucker";
+
+	printf("Then: %s\n\n", str1);
+	ft_memmove(str1, str1+4, 11);
+	printf("\nNow: %s\n", str1);
+	return 0;
 }
