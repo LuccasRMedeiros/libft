3509275a463b7a158fd9750bc5eeaba7c4422944
Luccas Rocigno de medeiros <lrocigno@c1r1p1.42sp.org.br>
2021-02-22 15:54:46 -0300

    Refactor

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 00:42:47 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/21 02:36:25 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/22 15:51:07 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t cnt)
 {
 	unsigned char	*dst_aux;
 	unsigned char	*src_aux;
 	size_t			i;
 
-	if ((!dst && !src) || n == 0)
+	if ((!dst && !src) || cnt == 0)
 		return (NULL);
 	dst_aux = (unsigned char*)dst;
 	src_aux = (unsigned char*)src;
 	i = 0;
-	while (i < n)
+	while (i < cnt)
 	{
 		dst_aux[i] = src_aux[i];
 		i++;
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 16:24:00 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/19 23:49:22 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/22 15:21:01 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	i = 0;
 	dst_aux = (unsigned char*)dst;
 	src_aux = (unsigned char*)src;
-	if (src_aux < dst_aux)
-		while (n-- > 0)
-			dst_aux[n] = src_aux[n];
-	else
-		return (ft_memcpy(dst, src, n));
+	while (n-- > 0)
+		dst_aux[n] = src_aux[n];
 	return (dst);
 }
