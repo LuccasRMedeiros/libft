38452266792d23f07873db0bc3ff2f7eee7378aa
Luccas Rocigno de medeiros <lrocigno@c1r1p1.42sp.org.br>
2021-02-23 01:33:12 -0300

    vish man

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 10:48:11 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/02/20 00:43:08 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/02/23 01:32:57 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	char	*str_aux;
-	int		i;
+	size_t	i;
 
-	str_aux = (char*)str;
 	i = 0;
-	while (str_aux)
+	printf("\n\033[1;31m -- Function -- \n\033[0m");
+	while (str[i - 1] != '\0')
 	{
-		if (*str_aux == c)
+		printf("Looking to: %i\n", str[i -1]);
+		if (str[i] == c)
 		{
-			return (str_aux);
+			return ((char*)str + i);
 		}
-		if (*str_aux == '\0')
-			break ;
-		str_aux++;
+		i++;
 	}
 	return (NULL);
 }
+
+int main()
+{
+	char *src = "the cake is a lie !\0I'm hidden lol\r\n";
+	char *d1 = strchr(src, ' ');
+	char *d2 = ft_strchr(src, ' ');
+
+	printf("d1: \033[1;33m%p - %s"
