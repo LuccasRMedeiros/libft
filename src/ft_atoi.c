16a04f2c3a6feccd43653d8494103d29bee02f2e
Luccas Rocigno de medeiros <lrocigno@c1r1p4.42sp.org.br>
2021-04-23 10:48:04 -0300

    everything ok

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 15:53:47 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/03/29 18:57:19 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/04/23 10:36:35 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ static bool	ignorable(char c)
 {
 	char	*space_list;
 
-	space_list = " +\t\r\n\v\f";
-	while (*space_list != '\0')
-	{
-		if (ft_strhvchr(space_list, &c))
-			return (true);
-		space_list++;
-	}
+	space_list = " \t\r\n\v\f";
+	if (ft_strhvchr(space_list, &c))
+		return (true);
 	return (false);
 }
 
@@ -33,24 +29,30 @@ static int	f_signal(int c)
 	return (1);
 }
 
+static long int	calc_am(long int am, int sig, char c)
+{
+	am = (am * 10) + (c - 48);
+	if (am > 2147483648)
+	{
+		if (sig < 0)
+			return (0);
+		return (-1);
+	}
+	return (am);
+}
+
 int	ft_atoi(const char *str)
 {
 	long int	am;
 	int			sig;
 
 	am = 0;
-	sig = 1;
+	sig = 0;
 	while (*str != '\0')
 	{
 		if (ft_isdigit(*str))
 		{
-			am = (am * 10) + (*str - 48);
-			if (am > 2147483648)
-			{
-				if (sig < 0)
-					return (0);
-				return (-1);
-			}
+			am = calc_am(am, sig, *str);
 		}
 		else if (*str == '-' && am == 0)
 			sig = f_signal(*str);
