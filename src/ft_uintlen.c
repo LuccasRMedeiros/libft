1119e41b45ad540e3a1079a8928c85f78a2af444
Luccas Rocigno <luccasrocigno@hotmail.com>
2021-04-30 21:43:42 -0300

    uintlen

--- src/ft_uintlen.c
+++ src/ft_uintlen.c
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_intlen.c                                        :+:      :+:    :+:   */
+/*   ft_uintlen.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/03 12:25:18 by lrocigno          #+#    #+#             */
+/*   Created: 2021/04/30 21:41:59 by lrocigno          #+#    #+#             */
 /*   Updated: 2021/04/30 21:43:27 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-size_t	ft_intlen(intmax_t n)
+size_t	ft_uintlen(uintmax_t n)
 {
 	size_t		len;
 
 	len = 1;
-	if (n < 0)
-	{
-		++len;
-		n *= -1;
-	}
 	while (n >= 10)
 	{
 		++len;
