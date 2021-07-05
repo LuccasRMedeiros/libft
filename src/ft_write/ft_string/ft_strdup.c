735165528732d81ed70d6c2137c5da5b09bc6267
Luccas <luccasrocigno@hotmail.com>
2021-07-04 21:08:49 -0300

    corrected libft calling

--- src/ft_write/ft_string/ft_strdup.c
+++ src/ft_write/ft_string/ft_strdup.c
@@ -6,11 +6,11 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 19:56:13 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/05/10 13:25:39 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/07/04 21:07:21 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <libft.h>
 #include <ft_string.h>
 
 char	*ft_strdup(const char *str)
