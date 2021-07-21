e4268974b88b0fe9f45ef6b6dc80d066b3bf8329
Luccas <luccasrocigno@hotmail.com>
2021-07-21 02:27:52 -0300

    strcmp with protection

--- src/ft_string/ft_strncmp.c
+++ src/ft_string/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 14:09:16 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/05/11 17:16:29 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/07/20 14:02:04 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	unsigned char	*ustr1;
-	unsigned char	*ustr2;
 	size_t			lstr1;
 	size_t			lstr2;
 	size_t			i;
 
-	ustr1 = (unsigned char *)str1;
-	ustr2 = (unsigned char *)str2;
 	lstr1 = ft_strlen(str1);
 	lstr2 = ft_strlen(str2);
 	i = 0;
+	if (str1 == NULL || str2 == NULL)
+		return (-1);
 	while (i < n && (i <= lstr1 && i <= lstr2))
 	{
-		if (ustr1[i] > ustr2[i])
+		if (str1[i] > str2[i])
 			return (1);
-		else if (ustr1[i] < ustr2[i])
+		else if (str1[i] < str2[i])
 			return (-1);
 		i++;
 	}
