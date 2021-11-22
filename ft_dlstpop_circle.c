3aa3e9c2bf2c03fec5c3a34218e013b9eb61ebdf
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2021-11-22 19:03:56 -0300

    new double linked lists operations

--- ft_dlstpop_circle.c
+++ ft_dlstpop_circle.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dlstadd_circle.c                                :+:      :+:    :+:   */
+/*   ft_dlstpop_circle.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/09 00:32:08 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/11/22 14:00:27 by lrocigno         ###   ########.fr       */
+/*   Created: 2021/11/22 14:01:05 by lrocigno          #+#    #+#             */
+/*   Updated: 2021/11/22 14:11:13 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Add a new item to the bottom of a circle double linked list. It consider the
-** informed node as the head.
+** Remove one node from the list and closes the circle again.
 */
 
-void	ft_dlstadd_circle(void *content, t_dlist **d_list)
+t_dlist	*ft_dlstpop_circle(t_dlist *dlst)
 {
-	t_dlist	*tail;
-	t_dlist	*add;
-
-	tail = (*d_list)->prev;
-	add = ft_dlstnew(content);
-	tail->next = add;
-	add->prev = tail;
-	add->next = *d_list;
-	(*d_list)->prev = add;
+	t_dlist	*next;
+	t_dlist	*prev;
 }
