e7cdeb442d9cfa3beadb04daa6e4fac3764cb41a
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2021-11-23 00:53:46 -0300

    corrected ft_dlstcircle

--- ft_dlstcircle.c
+++ ft_dlstcircle.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 20:50:15 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/11/22 23:43:48 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/11/22 23:48:37 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ t_dlist	*ft_dlstcircle(t_dlist *dlst)
 	t_dlist	*tail;
 
 	tail = ft_dlstlast(dlst);
+	dlst = ft_dlstfirst(dlst);
 	tail->next = dlst;
 	dlst->prev = tail;
 	return (dlst);
