cb62c4a6c98cd96c5a191ca847d1474323303c49
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2021-11-22 23:38:59 -0300

    dlst last corrected

--- ft_dlstlast.c
+++ ft_dlstlast.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 13:00:39 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/11/22 23:23:08 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/11/22 23:38:36 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** Return the last node of the list.
 */
 
-t_dlist	*ft_lstlast(t_dlist *dlst)
+t_dlist	*ft_dlstlast(t_dlist *dlst)
 {
 	if (!dlst)
 		return (NULL);
